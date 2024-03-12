b037471ded2cf5866f521406569976d38a961fc4
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-03-12 17:48:18 -0600

    fixed bug for notes.html not loading the css

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+
+require('./routes/api')(app);
+require('./routes/html')(app);
+
+app.listen(PORT, console.log(`App Listening on http://localhost:3001`));
